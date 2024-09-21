16df6e99f29ccb98bec99687786fbda2f29f58ff
MD Saiful Islam <saifulprogrammer@gmail.com>
2024-09-21 06:36:50 +0600

    Successfully replaced _AddToCart.cshtml through plugin

---
+++ src/Plugins/Nop.Plugin.Misc.ReplaceHomePage/Infrastructure/AddToCartViewEngine.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace Nop.Plugin.Misc.ReplaceHomePage.Infrastructure
+{
+    public class AddToCartViewEngine : IViewLocationExpander
+    {
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            if (context.AreaName == null && context.ViewName == "_AddToCart")
+            {
+                viewLocations = new[] { "~/Plugins/Misc.ReplaceHomePage/Views/Product/_AddToCart.cshtml" }.Concat(viewLocations);
+            }
+
+            return viewLocations;
+        }
+
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+        }
+    }
+}
