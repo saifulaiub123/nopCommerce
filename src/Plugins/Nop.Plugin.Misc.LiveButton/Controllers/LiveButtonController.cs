c7a19f0e03fa7e5666f1f2eb4e7d1473cf3751f0
MD Saiful Islam <saifulprogrammer@gmail.com>
2024-09-21 23:16:46 +0600

    Practice product live button plugin

---
+++ src/Plugins/Nop.Plugin.Misc.LiveButton/Controllers/LiveButtonController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Services.Security;
+using Nop.Web.Framework;
+using Nop.Web.Framework.Controllers;
+using Nop.Web.Framework.Mvc.Filters;
+
+namespace Nop.Plugin.Misc.WebApi.Frontend.Controllers;
+
+[AutoValidateAntiforgeryToken]
+[AuthorizeAdmin]
+[Area(AreaNames.ADMIN)]
+public class LiveButtonController : BasePluginController
+{
+    #region Fields
+
+    protected readonly IPermissionService _permissionService;
+
+    #endregion
+
+    #region Ctor 
+
+    public LiveButtonController(IPermissionService permissionService)
+    {
+        _permissionService = permissionService;
+    }
+
+    #endregion
+
+    #region Methods
+
+    [CheckPermission(StandardPermission.Configuration.MANAGE_PLUGINS)]
+    public virtual async Task<IActionResult> Configure()
+    {
+        return View("~/Plugins/Misc.ReplaceHomePage/Views/Configure/Configure.cshtml");
+    }
+
+    #endregion
+}
---
+++ src/Plugins/Nop.Plugin.Misc.LiveButton/Data/Migrations/SchemaMigration.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+using Nop.Data.Extensions;
+using Nop.Data.Migrations;
+using Nop.Plugin.Misc.LiveButton.Domain;
+
+namespace Nop.Plugin.Misc.LiveButton.Data.Migrations;
+
+[NopMigration("2024/09/21 08:45:00", "Nop.Plugin.Misc.LiveButton Schema", MigrationProcessType.Installation)]
+
+public class SchemaMigration : AutoReversingMigration
+{
+    public override void Up()
+    {
+        Create.TableFor<ProductDemo>();
+    }
+
+}
